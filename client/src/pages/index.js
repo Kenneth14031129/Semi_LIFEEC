336cd277036aa212e969ce5d5dd04b211e5a13a9
Kenneth14031129 <kenneth14031129@gmail.com>
2025-01-02 14:51:10 +0800

    Remove register

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,4 +2,3 @@ export { default as Dashboard } from "./Dashboard";
 export { default as HomeLayout } from "./HomeLayout";
 export { default as Landing } from "./Landing";
 export { default as Logout } from "./Logout";
-export { default as Register } from "./Register";
