4a4d4966c778129dfb410d831fae2da287482a07
Kenneth14031129 <kenneth14031129@gmail.com>
2025-01-06 13:27:01 +0800

    Configure deployment to GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Semi_LIFEEC/', // Updated to match your repository name
 })
